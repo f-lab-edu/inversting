4567d164ccf726f43ffa30d2ced9366063dd081d
Shin seongsu <tlstjdtn321@naver.com>
2023-07-28 21:17:34 +0900

    :sparkles: security userDetail 커스텀 설정

---
+++ core/core-api/src/main/java/com/flab/investing/support/security/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.flab.investing.support.security;
+
+import com.flab.investing.global.exception.custom.NotFoundUserIdException;
+import com.flab.investing.user.domain.User;
+import com.flab.investing.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) {
+        User user = userRepository.findByUserId(id)
+                .orElseThrow(() -> new NotFoundUserIdException());
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        return new org
+                .springframework
+                .security
+                .core
+                .userdetails
+                .User(user.getUserId(), user.getPassword(), grantedAuthorities);
+    }
+}
