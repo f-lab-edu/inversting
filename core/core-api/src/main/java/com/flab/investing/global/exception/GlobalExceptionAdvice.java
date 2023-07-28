5aab669071b67e58352a503c6f818ba9504a0892
Shin seongsu <tlstjdtn321@naver.com>
2023-07-28 21:13:07 +0900

    :sparkles: 공통 에러 추가

---
+++ core/core-api/src/main/java/com/flab/investing/global/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,32 @@
+package com.flab.investing.global.exception;
+
+import com.flab.investing.global.exception.dto.ExceptionCode;
+import com.flab.investing.global.exception.dto.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> validateException(MethodArgumentNotValidException e) {
+        String message = e.getFieldErrors().stream()
+                .map(error -> error.getField() + ":" + error.getDefaultMessage())
+                .collect(Collectors.joining("\n ,"));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(ExceptionCode.BAD_REQUEST.getCode(), message));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> customException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(ExceptionCode.CUSTOM_EXCEPTION.getCode(), e.getMessage()));
+    }
+
+}
