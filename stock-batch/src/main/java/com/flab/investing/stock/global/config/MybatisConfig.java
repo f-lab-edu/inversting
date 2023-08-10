eeac32d5426c17f154905a3263dedfc06922dcfb
Shin seongsu <tlstjdtn321@naver.com>
2023-08-10 21:52:25 +0900

    :sparkles: mybatis 설정 및 로그 설정

---
+++ stock-batch/src/main/java/com/flab/investing/stock/global/config/MybatisConfig.java
@@ -0,0 +1,26 @@
+package com.flab.investing.stock.global.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+        sessionFactory.setMapperLocations(resources);
+
+        return sessionFactory.getObject();
+    }
+
+}
