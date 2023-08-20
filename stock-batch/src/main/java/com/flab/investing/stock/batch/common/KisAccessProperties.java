df900ad28c8a49dd8586f9f2bc4d6769e182f0b6
Shin seongsu <tlstjdtn321@naver.com>
2023-08-20 20:07:26 +0900

    feat: kis Properties Setter 수정

--- stock-batch/src/main/java/com/flab/investing/stock/batch/common/KisAccessProperties.java
+++ stock-batch/src/main/java/com/flab/investing/stock/batch/common/KisAccessProperties.java
@@ -1,12 +1,15 @@
 package com.flab.investing.stock.batch.common;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @Component
-@ConfigurationProperties("kis")
+@ConfigurationProperties(prefix = "kis")
 public class KisAccessProperties {
 
     private String url;
