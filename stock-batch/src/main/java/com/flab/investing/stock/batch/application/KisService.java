4e275a0bb3bf7dabfaa3cada0bb7a50d8bc8a09e
Shin seongsu <tlstjdtn321@naver.com>
2023-08-20 20:06:50 +0900

    feat: 실시간 주식 시세 조회 dto 수정

--- stock-batch/src/main/java/com/flab/investing/stock/batch/application/KisService.java
+++ stock-batch/src/main/java/com/flab/investing/stock/batch/application/KisService.java
@@ -1,6 +1,7 @@
 package com.flab.investing.stock.batch.application;
 
 import com.flab.investing.stock.batch.common.KisAccessProperties;
+import com.flab.investing.stock.batch.domain.Stock;
 import com.flab.investing.stock.batch.domain.StockPrice;
 import com.flab.investing.stock.batch.domain.Token;
 import com.flab.investing.stock.batch.infrastructure.kis.KisTokenClient;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Service;
 public class KisService {
 
     private final static String GRANT_TYPE = "client_credentials";
-    private final static String MARKET_CODE = "주식";
+    private final static String MARKET_CODE = "J";
+    private final static String BEARER = "Bearer ";
+    private final static String TRACE_ID = "FHKST01010100";
 
     private final KisTokenClient kisTokenClient;
 
@@ -28,16 +31,22 @@ public class KisService {
         return new Token(token.access_token());
     }
 
-    public StockPrice getPrice(Token token, String code) {
-        KisPriceHeaderRequest header = new KisPriceHeaderRequest(token.getAccessToken(),
+    public StockPrice getPrice(Token token, Stock stock) {
+        KisPriceHeaderRequest header = new KisPriceHeaderRequest(BEARER + token.getAccessToken(),
                 kisAccessProperties.getAppKey(),
                 kisAccessProperties.getSecretKey(),
-                code);
+                TRACE_ID);
 
-        KisPriceResponse response = kisTokenClient.getPrice(header, MARKET_CODE, code);
+        KisPriceResponse response = kisTokenClient.getPrice(
+                kisAccessProperties.getSecretKey(),
+                kisAccessProperties.getAppKey(),
+                BEARER + token.getAccessToken(),
+                TRACE_ID,
+                MARKET_CODE,
+                stock.getShortCode().substring(1));
         KisPriceDetail detail = response.output();
 
-        return new StockPrice(code, detail.stck_prpr(), detail.iscd_stat_cls_code(),
+        return new StockPrice(stock.getId(),  stock.getShortCode(), detail.stck_prpr(), detail.iscd_stat_cls_code(),
                 detail.prdy_vrss_sign(), detail.acml_tr_pbmn(),
                 detail.acml_vol(), detail.stck_hgpr(), detail.stck_lwpr(),
                 detail.stck_mxpr(), detail.stck_llam());
