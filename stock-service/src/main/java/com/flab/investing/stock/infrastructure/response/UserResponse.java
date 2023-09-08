76153e59dd463de4e7e54f73d8a62962c7a091a9
shinseongsu <tlstjdtn321@naver.com>
2023-09-08 16:16:55 +0900

    feat: userToken 수정

--- stock-service/src/main/java/com/flab/investing/stock/infrastructure/response/UserResponse.java
+++ stock-service/src/main/java/com/flab/investing/stock/infrastructure/response/UserResponse.java
@@ -2,6 +2,7 @@ package com.flab.investing.stock.infrastructure.response;
 
 public record UserResponse(
         String code,
+        Long userId,
         String message,
         String name
 ) {
