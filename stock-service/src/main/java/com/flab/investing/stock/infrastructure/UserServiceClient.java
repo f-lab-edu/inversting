cd26b70e55ed9c04a4c10aef452ccd8456a34dda
shinseongsu <tlstjdtn321@naver.com>
2023-09-08 17:10:20 +0900

    feat: user 조회하는거 get으로 수정

--- stock-service/src/main/java/com/flab/investing/stock/infrastructure/UserServiceClient.java
+++ stock-service/src/main/java/com/flab/investing/stock/infrastructure/UserServiceClient.java
@@ -3,13 +3,14 @@ package com.flab.investing.stock.infrastructure;
 import com.flab.investing.stock.infrastructure.request.UserRequest;
 import com.flab.investing.stock.infrastructure.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "userService", url="${userService.url}")
 public interface UserServiceClient {
 
-    @PostMapping("/user/validate")
+    @GetMapping("/users")
     UserResponse tokenValidate(@RequestBody UserRequest request);
 
 }
