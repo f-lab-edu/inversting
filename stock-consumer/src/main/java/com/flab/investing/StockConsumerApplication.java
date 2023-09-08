d00539f3b3b59df3247b2fd341292aa2581d3fb6
shinseongsu <tlstjdtn321@naver.com>
2023-09-08 16:05:53 +0900

    chore: 기본 설정 추가

---
+++ stock-consumer/src/main/java/com/flab/investing/StockConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.flab.investing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockConsumerApplication.class, args);
+    }
+
+}
---
+++ stock-consumer/src/test/java/com/flab/stockconsumer/StockConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.stockconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
