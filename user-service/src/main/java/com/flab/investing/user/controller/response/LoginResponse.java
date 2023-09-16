28feb561ae46b1cfc5154e6b67a56581b5ab0479
Shin seongsu <tlstjdtn321@naver.com>
2023-09-16 18:52:03 +0900

    refactor: 로그인시, 코드 및 메시지 추가

--- user-service/src/main/java/com/flab/investing/user/controller/response/LoginResponse.java
+++ user-service/src/main/java/com/flab/investing/user/controller/response/LoginResponse.java
@@ -1,6 +1,8 @@
 package com.flab.investing.user.controller.response;
 
 public record LoginResponse(
+        String code,
+        String message,
         String accessToken,
         String refreshToken
 ) {}
