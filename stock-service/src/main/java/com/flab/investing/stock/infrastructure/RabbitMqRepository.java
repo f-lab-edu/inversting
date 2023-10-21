f04ff2f9608089dd1d8a4d4b14050fd24fd000f3
Shin seongsu <tlstjdtn321@naver.com>
2023-10-21 17:40:10 +0900

    feat: circuitbreaker 기능 추가

--- stock-service/src/main/java/com/flab/investing/stock/infrastructure/RabbitMqRepository.java
+++ stock-service/src/main/java/com/flab/investing/stock/infrastructure/RabbitMqRepository.java
@@ -1,6 +1,8 @@
 package com.flab.investing.stock.infrastructure;
 
+import com.flab.investing.global.error.exception.RabbitMqRetryException;
 import com.flab.investing.stock.application.dto.TradeRequest;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -21,9 +23,15 @@ public class RabbitMqRepository {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Retry(name = "rabbitmqRetry", fallbackMethod = "rabbitmqFallback")
     public void oderSend(TradeRequest tradeRequest) {
         log.info("message sent: {}", tradeRequest);
         rabbitTemplate.convertAndSend(exchangeName, routingKey, tradeRequest);
+    }
+
+    private void rabbitmqFallback(TradeRequest tradeRequest, Exception exception) {
+        log.error("주식 주문 요청에 실패하였습니다. : {}" , tradeRequest);
+        throw new RabbitMqRetryException(exception.getMessage());
     }
 
 }
