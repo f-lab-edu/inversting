09767f74ef0495539303fedc23805261ea34e7e7
Shin seongsu <tlstjdtn321@naver.com>
2023-10-24 01:09:57 +0900

    feat: spring-config cloud 사용하여, yml 정보 나타내기

---
+++ stock-config/stock-config/src/main/java/com/flab/investing/global/config/ConfigServerConfiguration.java
@@ -0,0 +1,9 @@
+package com.flab.investing.global.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfiguration {
+}
