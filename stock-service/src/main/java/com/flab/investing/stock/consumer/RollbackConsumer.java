b801ce011728dbed1d56127341218d95e084b2ea
Shin seongsu <tlstjdtn321@naver.com>
2023-11-12 17:15:58 +0900

    feat: rollback 서비스 json 파싱 추가

--- stock-service/src/main/java/com/flab/investing/stock/consumer/RollbackConsumer.java
+++ stock-service/src/main/java/com/flab/investing/stock/consumer/RollbackConsumer.java
@@ -1,20 +1,37 @@
 package com.flab.investing.stock.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flab.investing.global.error.exception.SerializerException;
 import com.flab.investing.stock.application.TradeService;
 import com.flab.investing.stock.consumer.dto.TradeException;
+import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RollbackConsumer {
 
     private final TradeService tradeService;
+    private final ObjectMapper objectMapper;
 
-    @RabbitListener(queues = "${rabbitmq.queue.rollback.name}")
-    public void tradeRollback(TradeException tradeException) {
+    @SqsListener("${cloud.sqs.rollback.queue.name}")
+    public void tradeRollback(String message) {
+        log.info("message : {}", message);
+        TradeException tradeException = this.readValue(message, TradeException.class);
         tradeService.rollbackTrade(tradeException.tradeId());
+    }
+
+    private <T> T readValue(String message, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(message, clazz);
+        } catch (JsonProcessingException exception) {
+            log.error("에러난 메시지 : {}", message);
+            throw new SerializerException();
+        }
     }
 
 }
