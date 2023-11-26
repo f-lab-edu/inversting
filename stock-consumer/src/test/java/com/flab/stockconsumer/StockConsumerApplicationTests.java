cf9589b7f415834375bc994c3c8ca09ac3e3b324
Shin seongsu <tlstjdtn321@naver.com>
2023-11-26 17:08:09 +0900

    test: SpringBootTest 삭제

--- stock-consumer/src/test/java/com/flab/stockconsumer/StockConsumerApplicationTests.java
+++ stock-consumer/src/test/java/com/flab/stockconsumer/StockConsumerApplicationTests.java
@@ -3,7 +3,6 @@ package com.flab.stockconsumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class StockConsumerApplicationTests {
 
     @Test
